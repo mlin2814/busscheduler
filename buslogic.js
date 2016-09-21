03569acc2489bbb57ecc0c5743da8a7d1e7f8ff6
Matthew Lin <Matthew Lin>
2016-09-21 11:35:20 -0400

    Modified time conversion logic

--- buslogic.js
+++ buslogic.js
@@ -27,6 +27,17 @@ $("#addBusBtn").on("click", function(){
 		rate: busRate
 	};
 
+	var today = new Date();
+	var thisMonth = today.getMonth() + 1;
+	var thisDate = today.getDate();
+	var thisYear = today.getFullYear();
+
+// DATE STRING //
+
+  var dateString = "";
+  var dateString = dateString.concat(thisMonth, "/", thisDate, "/", thisYear);
+
+
 	// Uploads employee data to the database
 	database.ref().push(newBus);
 
@@ -77,6 +88,50 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey){
 	// Calculate the total billed rate
 	// var empBilled = empMonths * empRate;
 	// console.log(empBilled);
+
+    var busFirstArrivalTime = value.busStart;
+    
+// VARIABLE CALCULATIONS //
+
+    var busNextDeparture;
+    var minutesAway;
+
+// MOMENT //
+
+    var convertedDate = moment(new Date(busFirstArrivalTime));
+    
+// MINUTES AWAY //
+
+    var minuteDiffFirstArrivalToNow = moment(convertedDate).diff( moment(), "minutes")*(-1);
+
+      // Check for New Train Times //
+      
+      if(minuteDiffFirstArrivalToNow <= 0){
+
+        // Current Departure //
+
+        minutesAway = moment(convertedDate).diff( moment(), "minutes");
+
+        // Next Depature Time //
+
+        busNextDepartureDate = convertedDate;
+
+      }
+
+      else{
+
+        // Next Train Departure //
+
+        busMinutesAway = busRate - (minuteDiffFirstArrivalToNow % busRate);
+
+        // Next Departure Time //
+
+        var trainNextDepartureDate = moment().add(trainMinutesAway, 'minutes');
+      }
+
+// AM/PM //
+
+    trainNextDeparture = trainNextDepartureDate.format("hh:mm A");
 
 	// Add each train's data into the table
 	$("#busTable > tbody").append("<tr><td>" + busName + "</td><td>" + busPlace + "</td><td>" + busStartPretty + "</td><td>" + arrival + "</td><td>" + busRate + "</td><td>");
